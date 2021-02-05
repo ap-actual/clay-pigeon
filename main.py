20db29f735915f1652f8e8e6b7f0a7ec42f02c2a
ap-actual <78578795+ap-actual@users.noreply.github.com>
2021-02-04 23:22:12 -0500

    Thursday night sign-off
    Went a little harder than I thought I would tonight, now reading reference ticks, and storing single target cost-function matrix into array in the main. Just blocks of code, maybe one day I'll add comments to this thing...

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from data_pull import foo
+from numpy import loadtxt
+import numpy as np
 
-x = foo('MSFT')
+lines = []
 
-print(x)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+file = open("./reference.dat")
+lines = file.read().split('\n')
+file.close()
+print(lines)
+
+diff = np.zeros((15, 3, len(lines)))
+n = 0
+for tick in lines:
+    x = foo(tick)
+    diff[0:len(x[:,0]), :, n] = x
+    n=n+1
+    print(n)
