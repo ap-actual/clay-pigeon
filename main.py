e97c6d00009c43e0a3fe4104a0073cdb16865105
ap-actual <78578795+ap-actual@users.noreply.github.com>
2021-02-06 18:54:20 -0500

    Saturday night commit
    Begin polishing absolute min finder, and print the amin to the terminal

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from data_pull import get_diff
 import numpy as np
-from progressbar import ProgressBar
+import progressbar
 
-prgbar = ProgressBar()  # initialize progress bar
+# setup progress bar parameters
+widgets=[
+    ' [', progressbar.Timer(), '] ',
+    progressbar.Bar(),
+    ' (', progressbar.ETA(), ') ',
+]
 
-# define weights of analysis (these MUST add up to 1
+# define weights of analysis (these MUST add up to 1)
 w_pl = 0.33
 w_ph = 0.33
 w_pc = 0.34
@@ -21,18 +26,25 @@ tar_ticks = file.read().split('\n')
 file.close()
 print(tar_ticks)
 
+# initialize difference array
+diff = np.zeros((15, 6, len(ref_ticks)))
 
-diff = np.zeros((15, 6, len(ref_ticks)))  # initialize difference array
-
+# loop through reference ticks
 n = 0  # loop counter
-for tick in prgbar(ref_ticks):
+for tick in progressbar.progressbar(ref_ticks, widgest=widgets):
     x = get_diff(tick)  # call get_diff from data_pull.py & cast into variable
     diff[0:len(x[:, 1:]), 0:4, n] = x  # find size of variable, and cast into diff array
     diff[0:len(x[:, 1:]), 4, n] = diff[0:len(x[:, 1:]), 1, n]*w_ph + diff[0:len(x[:, 2:]), 3, n]*w_pl + diff[0:len(x[:, 1:]), 3, n]*w_pc  # apply weights to differences, create new column with score
     n = n+1
-print(diff[:, 1:5, n-1])
-masked_diff = np.ma.masked_equal(diff[:,4,:], 0.0, copy=False)
-print(np.amin(masked_diff))
-print(np.where(masked_diff == np.amin(masked_diff)))
-print(diff[7, 4, 4])
-print(diff[7,0,4])
+
+# Find absolute minimum and location of minimum
+masked_diff = np.ma.masked_equal(diff[:, 4, :], 0.0, copy=False)
+diff_min = np.amin(masked_diff)
+diff_imin = np.where(masked_diff == np.amin(masked_diff))
+
+
+print('Most similar stock: ')
+print(ref_ticks[diff_imin[1][0]])
+print(diff[diff_imin[0][0], 0, diff_imin[1][0]]) # reference diff[min year, year col, diff stock tick]
+print('Similarity:')
+print(diff_min)
