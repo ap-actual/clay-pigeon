f31eb1531051a62b0878fdf6ca3eb2e8404cdf7a
ap-actual <78578795+ap-actual@users.noreply.github.com>
2021-02-07 14:00:47 -0500

    Plotting function
    Created new plotting function to plot comparisons

--- data_plotter.py
+++ data_plotter.py
@@ -1,41 +1,33 @@
-# script to pull data from yfinance
-
-# Import the plotting library
-import matplotlib.pyplot as plt
-import pandas as pd
 import yfinance as yf
 import numpy as np
 import datetime as dt
+import matplotlib.pyplot as plt
 
-def get_diff(ref_tick, tar_tick, trade_days_prior, td_year, td_month, td_day):
+
+def plot_data(ref_tick, target_tick, trade_days_prior, td_year, td_month, td_day, rf_year):
 
     # Get day of week & week of year
     td_week = dt.date(td_year, td_month, td_day).isocalendar()[1]
     td_day_of_week = dt.date(td_year,td_month,td_day).weekday()
 
-
     # Get the data of target stock
     # TODO  put in error handling
-    tick = yf.Ticker(tar_tick)
+    tick = yf.Ticker(target_tick)
     target = tick.history("1y")
 
     # Get data of reference
     # TODO put in error handling
     tick = yf.Ticker(ref_tick)
-    reference = tick.history("10y")
+    reference = tick.history(start=dt.datetime(year=int(rf_year-1), month=td_month, day=td_day),
+                             end=dt.datetime(year=int(rf_year), month=td_month, day=td_day))
 
-
-    #Index(['Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'], dtype='object')
-
-    # normalize target high & low values to percent of opening values
+    # normalize target high & low values to percent of opening values, create new columns for 'day of week' and 'week of year'
     target['percent_high'] = np.where(target['High'] < 1, target['High'], target['High']/target['Open'])
     target['percent_low']= np.where(target['Low'] < 1, target['Low'], target['Low']/target['Open'])
     target['percent_close']= np.where(target['Close'] < 1, target['Close'], target['Close']/target['Open'])
-    # create new columns for 'day of week' and 'week of year'
     target['Date'] = target.index
     target['day_of_week'] = target['Date'].apply(lambda x: x.weekday())
     target['week_num']=target['Date'].dt.isocalendar().week
-
 
     # do same as above but for target
     reference['percent_high'] = np.where(reference['High'] < 1, reference['High'], reference['High']/reference['Open'])
@@ -44,14 +36,18 @@ def get_diff(ref_tick, tar_tick, trade_days_prior, td_year, td_month, td_day):
     reference['Date'] = reference.index
     reference['day_of_week'] = reference['Date'].apply(lambda x: x.weekday())
     reference['week_num']=reference['Date'].dt.isocalendar().week
-    reference = reference[(reference['Date'].dt.year) != td_year]
-
 
     # find nth day prior to analysis of target
-    ref_end_ii = np.where(np.logical_and(reference['day_of_week'] == td_day_of_week-1, reference['week_num'] == td_week))
-    ref_end_i = ref_end_ii[0][1:]
-    print(ref_end_i)
-    tar_end_ii = np.where(np.logical_and(target['day_of_week'] == td_day_of_week-1, target['week_num'] == td_week))
+    ref_end_ii = np.where(
+        np.logical_and(reference['day_of_week'] == td_day_of_week-1, reference['week_num'] == td_week))
+
+    if len(ref_end_ii) > 1:
+        ref_end_i = ref_end_ii[0][len(ref_end_ii)]
+    else:
+        ref_end_i = ref_end_ii[0][0]
+    ref_start_i = ref_end_i - trade_days_prior
+
+    tar_end_ii = np.where(np.logical_and(target['day_of_week'] == td_day_of_week - 1, target['week_num'] == td_week))
     tar_end_i = tar_end_ii[0]
     tar_start_i = tar_end_i - trade_days_prior
 
@@ -61,18 +57,18 @@ def get_diff(ref_tick, tar_tick, trade_days_prior, td_year, td_month, td_day):
     tar_dat_arr_pl = tar_dat['percent_low'].to_numpy()
     tar_dat_arr_pc = tar_dat['percent_close'].to_numpy()
 
-    diff = np.ones((len(ref_end_i),4))
-    n = 0
-    for i in ref_end_i:
-        ref_start_i = i - trade_days_prior
-        ref_dat = reference.iloc[ref_start_i:i, :]
-        diff[n,0] = ref_dat.iloc[0]['Date'].year
-        ref_dat_arr = ref_dat['percent_high'].to_numpy()
-        diff[n,1] = sum(abs(ref_dat_arr - tar_dat_arr_ph))
-        ref_dat_arr = ref_dat['percent_low'].to_numpy()
-        diff[n,2] = sum(abs(ref_dat_arr - tar_dat_arr_pl))
-        ref_dat_arr = ref_dat['percent_close'].to_numpy()
-        diff[n,3] = sum(abs(ref_dat_arr - tar_dat_arr_pc))
-        n = n+1
+    ref_dat = reference.iloc[ref_start_i:ref_end_i, :]
 
-    return diff
+    ref_dat_arr_ph = ref_dat['percent_high'].to_numpy()
+    ref_dat_arr_pl = ref_dat['percent_low'].to_numpy()
+    ref_dat_arr_pc = ref_dat['percent_close'].to_numpy()
+
+    t1 = list(range(1, int(tar_end_i - tar_start_i+1)))
+    t2 = list(range(1, int(ref_end_i - ref_start_i + 1)))
+    plt.plot(t1, tar_dat_arr_pc)
+    plt.plot(t2, ref_dat_arr_pc)
+    plt.minorticks_on()
+    plt.grid(which='major')
+    plt.legend([target_tick, ref_tick + ' - ' + str(rf_year)])
+    plt.show()
+    return 0
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data_pull import get_diff
+from data_plotter import plot_data
 import numpy as np
 import progressbar
 
@@ -9,13 +10,23 @@ widgets=[
     ' (', progressbar.ETA(), ') ',
 ]
 
+# algorithm parameters
+years_to_check = 10  # how many years to run analysis over
+trade_days_prior = 7  # how many prior trading days to compare against
+
+# target and reference dates
+td_year = 2021
+td_month = 2
+td_day = 5
+
+
 # define weights of analysis (these MUST add up to 1)
 w_pl = 0.33
 w_ph = 0.33
 w_pc = 0.34
 
 # import reference ticks from file
-file = open("./reference.dat")
+file = open("./reference2.dat")
 ref_ticks = file.read().split('\n')
 file.close()
 print(ref_ticks)
@@ -24,27 +35,34 @@ print(ref_ticks)
 file = open("./target.dat")
 tar_ticks = file.read().split('\n')
 file.close()
-print(tar_ticks)
+
+# TODO change this to use file targets
+tar_tick = 'GOOGL'
 
 # initialize difference array
-diff = np.zeros((15, 6, len(ref_ticks)))
+diff = np.zeros((10, 6, len(ref_ticks)))
 
 # loop through reference ticks
 n = 0  # loop counter
-for tick in progressbar.progressbar(ref_ticks, widgest=widgets):
-    x = get_diff(tick)  # call get_diff from data_pull.py & cast into variable
-    diff[0:len(x[:, 1:]), 0:4, n] = x  # find size of variable, and cast into diff array
-    diff[0:len(x[:, 1:]), 4, n] = diff[0:len(x[:, 1:]), 1, n]*w_ph + diff[0:len(x[:, 2:]), 3, n]*w_pl + diff[0:len(x[:, 1:]), 3, n]*w_pc  # apply weights to differences, create new column with score
-    n = n+1
+for ref_tick in progressbar.progressbar(ref_ticks, widgest=widgets):
+    try:
+        x = get_diff(ref_tick, tar_tick, trade_days_prior, td_year, td_month, td_day)  # call get_diff from data_pull.py & cast into variable
+        diff[0:len(x[:, 1:]), 0:4, n] = x  # find size of variable, and cast into diff array
+        diff[0:len(x[:, 1:]), 4, n] = diff[0:len(x[:, 1:]), 1, n]*w_ph + diff[0:len(x[:, 2:]), 3, n]*w_pl + diff[0:len(x[:, 1:]), 3, n]*w_pc  # apply weights to differences, create new column with score
+        n = n + 1
+    except:
+        n = n + 1
+        pass
 
 # Find absolute minimum and location of minimum
 masked_diff = np.ma.masked_equal(diff[:, 4, :], 0.0, copy=False)
 diff_min = np.amin(masked_diff)
 diff_imin = np.where(masked_diff == np.amin(masked_diff))
 
-
 print('Most similar stock: ')
 print(ref_ticks[diff_imin[1][0]])
 print(diff[diff_imin[0][0], 0, diff_imin[1][0]]) # reference diff[min year, year col, diff stock tick]
 print('Similarity:')
 print(diff_min)
+
+plot_data(ref_ticks[diff_imin[1][0]], tar_tick, trade_days_prior, td_year, td_month, td_day, diff[diff_imin[0][0], 0, diff_imin[1][0]])
